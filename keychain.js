f845bdc09bcefbc1a37c0f1305ae4c422c81cd68
David McFadzean <davidmc@gmail.com>
2024-01-12 13:41:46 -0500

    Added DID doc template

--- keychain.js
+++ keychain.js
@@ -2,6 +2,7 @@ import { createHelia } from 'helia';
 import { json } from '@helia/json';
 import { FsBlockstore } from 'blockstore-fs';
 import { CID } from 'multiformats/cid';
+import fs from 'fs';
 
 const blockstore = new FsBlockstore('./ipfs');
 
@@ -20,13 +21,23 @@ async function resolveDid(did) {
         const cid = CID.parse(suffix);
         console.log(cid.toString());
         const j = json(helia);
-        const obj = await j.get(cid);
-        return obj;
+        const keys = await j.get(cid);
+        const template = fs.readFileSync('did-doc.template');
+        const doc = JSON.parse(template);
+
+        doc.didDocument.id = did;
+        doc.didDocument.verificationMethod[0].controller = did;
+        doc.didDocument.verificationMethod[0].publicKeyJwk = keys;
+        doc.didDocumentMetadata.canonicalId = did;
+
+        return JSON.stringify(doc, null, 4);
     }
     catch (error) {
         console.log(error);
     }
-    helia.stop();
+    finally {
+        helia.stop();
+    }
 }
 
 export {
