fc81190c2fa402db5b1ddbeb4cddcc2c72563878
David McFadzean <davidmc@gmail.com>
2024-01-19 14:32:18 -0500

    Removed debug logs

--- cipher.js
+++ cipher.js
@@ -68,16 +68,9 @@ function verifySig(msgHash, sigHex, publicJwk) {
 }
 
 function encryptMessage(pubKey, privKey, message) {
-
-    console.log('encryptMessage pub', pubKey);
-    console.log('encryptMessage prv', privKey);
-
     const priv = base64url.baseDecode(privKey.d);
     const pub = convertJwkToCompressedBytes(pubKey);
     const ss = secp.getSharedSecret(priv, pub);
-
-    console.log('encryptMessage ss', ss);
-
     const key = ss.slice(0, 32);
     const chacha = managedNonce(xchacha20poly1305)(key); // manages nonces for you
     const data = utf8ToBytes(message);
@@ -87,16 +80,9 @@ function encryptMessage(pubKey, privKey, message) {
 }
 
 function decryptMessage(pubKey, privKey, ciphertext) {
-
-    console.log('decryptMessage pub', pubKey);
-    console.log('decryptMessage prv', privKey);
-
     const priv = base64url.baseDecode(privKey.d);
     const pub = convertJwkToCompressedBytes(pubKey);
     const ss = secp.getSharedSecret(priv, pub);
-
-    console.log('decryptMessage ss', ss);
-
     const key = ss.slice(0, 32);
     const chacha = managedNonce(xchacha20poly1305)(key); // manages nonces for you
     const cipherdata = base64url.baseDecode(ciphertext);
