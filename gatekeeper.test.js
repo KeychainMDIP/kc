6d5b1ff26e5e1509c20222ef0b3c655bc6df3d0f
David McFadzean <davidmc@gmail.com>
2024-02-01 15:55:53 -0500

    Added another test for resolveDid

--- gatekeeper.test.js
+++ gatekeeper.test.js
@@ -36,7 +36,7 @@ describe('resolveDid', () => {
         mockFs.restore();
     });
 
-    it('should resolve a valid DID with a valid Document', async () => {
+    it('should resolve a valid DID with a valid data anchor', async () => {
         mockFs({});
 
         const mockAnchor = { controller: "mockController", data: "mockAnchor" };
@@ -47,5 +47,24 @@ describe('resolveDid', () => {
         expect(doc.didDocument.controller).toBe(mockAnchor.controller);
         expect(doc.didDocumentMetadata.mdip.version).toBe(1);
         expect(doc.didDocumentMetadata.data).toBe(mockAnchor.data);
+    });
+
+    it('should resolve a valid DID with a valid agent anchor (pubkey)', async () => {
+        mockFs({});
+
+        const mockAnchor = {
+            crv: "secp256k1",
+            kty: "EC",
+            x: "ZGMyLSKEqUBFZvRb7cv_3Kyw6TSCaHMe2z8A5aDot8M",
+            y: "CePPcnEVNq7WvwbxnaXI2ekGZJHSFoqFnrXryzxqMMI"
+        };
+
+        const did = await gatekeeper.generateDid(mockAnchor);
+        const doc = JSON.parse(await gatekeeper.resolveDid(did));
+
+        expect(doc.didDocument.id).toBe(did);
+        expect(doc.didDocument.verificationMethod.length).toBe(1);
+        expect(doc.didDocument.verificationMethod[0].publicKeyJwk).toStrictEqual(mockAnchor);
+        expect(doc.didDocumentMetadata.mdip.version).toBe(1);
     });
 });
