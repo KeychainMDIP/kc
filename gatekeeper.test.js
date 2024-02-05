d4dbf0ef82adb0a2487b51eaeaa8d80521c0a7a6
David McFadzean <davidmc@gmail.com>
2024-02-05 16:02:47 -0500

    Added unit tests for generateDid

--- gatekeeper.test.js
+++ gatekeeper.test.js
@@ -2,17 +2,32 @@ import fs from 'fs';
 import mockFs from 'mock-fs';
 import * as gatekeeper from './gatekeeper.js';
 
-describe('createDid', () => {
+describe('generateDid', () => {
 
     afterEach(() => {
         mockFs.restore();
     });
 
+    it('should create DID from txn', async () => {
+        const mockTxn = "mockTxn";
+        const did = await gatekeeper.generateDid(mockTxn);
 
-    it('should create DID from an object anchor', async () => {
-        mockFs({});
+        expect(did.length).toBe(60);
+        expect(did.startsWith('did:mdip:'));
+    });
 
-        expect(true).toBe(true);
+    it('should create different DIDs from same txn', async () => {
+        const mockTxn = "mockTxn";
+        const did1 = await gatekeeper.generateDid(mockTxn);
+        const did2 = await gatekeeper.generateDid(mockTxn);
+
+        expect(did1.length).toBe(60);
+        expect(did1.startsWith('did:mdip:'));
+
+        expect(did2.length).toBe(60);
+        expect(did2.startsWith('did:mdip:'));
+
+        expect(did1 !== did2).toBe(true);
     });
 
 });
