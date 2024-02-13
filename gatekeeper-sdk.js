360f96c4a2eee6890f09c96dc61abceec0a20878
David McFadzean <davidmc@gmail.com>
2024-02-13 09:27:59 -0500

    Added error handling

--- gatekeeper-sdk.js
+++ gatekeeper-sdk.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const URL = 'http://localhost:3000';
+const connectionError = `Can't connect to gatekeeper. Is server running on ${URL}?`;
 
 export async function start() {
 }
@@ -10,50 +11,56 @@ export async function stop() {
 
 export async function getPeerId() {
     try {
-        const getPeerId = await axios.get(`${URL}/peerid`);
-        return getPeerId.data;
+        const response = await axios.get(`${URL}/peerid`);
+        return response.data;
     }
     catch (error) {
-
+        throw connectionError;
     }
 }
 
 export async function createDID(txn) {
     try {
-        const getDid = await axios.post(`${URL}/did/`, txn);
-        return getDid.data;
+        const response = await axios.post(`${URL}/did/`, txn);
+        return response.data;
     }
     catch (error) {
-
+        throw connectionError;
     }
 }
 
 export async function resolveDID(did, asof = null) {
     try {
-        const getDoc = await axios.get(`${URL}/did/${did}?asof=${asof}`);
-        return getDoc.data;
+        if (asof) {
+            const response = await axios.get(`${URL}/did/${did}?asof=${asof}`);
+            return response.data;
+        }
+        else {
+            const response = await axios.get(`${URL}/did/${did}`);
+            return response.data;
+        }
     }
     catch (error) {
-
+        throw connectionError;
     }
 }
 
 export async function updateDID(txn) {
     try {
-        const getDid = await axios.post(`${URL}/did/${txn.did}`, txn);
-        return getDid.data;
+        const response = await axios.post(`${URL}/did/${txn.did}`, txn);
+        return response.data;
     }
     catch (error) {
-
+        throw connectionError;
     }
 }
 
 export async function deleteDID(txn) {
     try {
-        const getDid = await axios.delete(`http://localhost:3000/did/${txn.did}`, { data: txn });
-        return getDid.data;
+        const response = await axios.delete(`http://localhost:3000/did/${txn.did}`, { data: txn });
+        return response.data;
     }
     catch (error) {
-
+        throw connectionError;
     }
 }
