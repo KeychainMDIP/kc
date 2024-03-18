98e491ed4759dbb691ffb9826607865446746974
David McFadzean <davidmc@gmail.com>
2024-03-18 13:40:18 -0400

    Added verifyDb

--- server.js
+++ server.js
@@ -43,27 +43,27 @@ app.get('/did/:did', async (req, res) => {
 
 app.get('/explore/:did', async (req, res) => {
     try {
-	const doc = await gatekeeper.resolveDID(req.params.did, req.query.asof);
-	var hthead = '<html><body>';
-	hthead = hthead + '<h1>MDIP Network Explorer</h1>';
-	hthead = hthead + '<table><tr><td><h3>' + req.params.did + '</h3></td>';
-	var htdoc = JSON.stringify(doc,null,5);
-	htdoc = htdoc.replace(/"didDocument"/g, '"<b>didDocument</b>"');
-	htdoc = htdoc.replace(/"didDocumentMetadata"/g, '"<b>didDocumentMetadata</b>"');
-	htdoc = htdoc.replace(/"manifest"/g, '"<b>manifest</b>"');
-	htdoc = htdoc.replace(/"issuer"/g, '"<b>issuer</b>"');
-	htdoc = htdoc.replace(/"signer"/g, '"<b>signer</b>"');
-	htdoc = htdoc.replace(/"id"/g, '"<b>id</b>"');
-	htdoc = htdoc.replace(/"credential"/g, '"<b>credential</b>"');
-	htdoc = htdoc.replace(/"vault"/g, '"<b>vault</b>"');
-	htdoc = htdoc.replace(/"(did:mdip:.*)"/g, '"<a href="/explore/$1">$1</a>"');
-	htdoc = hthead + '<tr><td><hr><pre>' + htdoc + '</pre><hr></td></tr>';
-	var now = new Date();
-	htdoc = htdoc + '</table>' + now + '</body></html>';
-	res.send(htdoc);
-    } catch (error ) {
-	console.error(error);
-	res.status(500).send(error.toString());
+        const doc = await gatekeeper.resolveDID(req.params.did, req.query.asof);
+        var hthead = '<html><body>';
+        hthead = hthead + '<h1>MDIP Network Explorer</h1>';
+        hthead = hthead + '<table><tr><td><h3>' + req.params.did + '</h3></td>';
+        var htdoc = JSON.stringify(doc, null, 5);
+        htdoc = htdoc.replace(/"didDocument"/g, '"<b>didDocument</b>"');
+        htdoc = htdoc.replace(/"didDocumentMetadata"/g, '"<b>didDocumentMetadata</b>"');
+        htdoc = htdoc.replace(/"manifest"/g, '"<b>manifest</b>"');
+        htdoc = htdoc.replace(/"issuer"/g, '"<b>issuer</b>"');
+        htdoc = htdoc.replace(/"signer"/g, '"<b>signer</b>"');
+        htdoc = htdoc.replace(/"id"/g, '"<b>id</b>"');
+        htdoc = htdoc.replace(/"credential"/g, '"<b>credential</b>"');
+        htdoc = htdoc.replace(/"vault"/g, '"<b>vault</b>"');
+        htdoc = htdoc.replace(/"(did:mdip:.*)"/g, '"<a href="/explore/$1">$1</a>"');
+        htdoc = hthead + '<tr><td><hr><pre>' + htdoc + '</pre><hr></td></tr>';
+        var now = new Date();
+        htdoc = htdoc + '</table>' + now + '</body></html>';
+        res.send(htdoc);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error.toString());
     }
 });
 
@@ -123,8 +123,16 @@ app.post('/merge', async (req, res) => {
 
 const port = 3000;
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+gatekeeper.verifyDb().then((invalid) => {
+    if (invalid === 0) {
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    }
+    else {
+        console.log(`${invalid} invalid DIDs in MDIP db`);
+        process.exit();
+    }
 });
 
 process.on('uncaughtException', (error) => {
