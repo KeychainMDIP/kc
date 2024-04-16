2261ac23369286388e78acc080fd2dacf1ce3fd2
David McFadzean <davidmc@gmail.com>
2024-04-16 14:44:28 -0400

    Added fetchTransaction

--- bitcoin.js
+++ bitcoin.js
@@ -9,21 +9,60 @@ const client = new BtcClient({
     port: config.btcPort,
 });
 
-async function fetchLatestBlock() {
+async function fetchTransaction(txnid) {
     try {
-        // Get block count (height)
-        const blockCount = await client.getBlockCount();
+        const txn = await client.getTransactionByHash(txnid);
+        const asm = txn.vout[0].scriptPubKey.asm;
 
+        if (asm.startsWith('OP_RETURN')) {
+            const hexString = asm.slice(10);
+            const textString = Buffer.from(hexString, 'hex').toString('utf8');
+
+            if (textString.startsWith('Qm')) {
+                console.log(txnid);
+                console.log(asm);
+                console.log(textString);
+            }
+        }
+    }
+    catch (error) {
+        console.error(`Error fetching txn: ${error}`);
+    }
+}
+
+async function fetchBlock(height) {
+    try {
         // Get block hash
-        const blockHash = await client.getBlockHash(blockCount);
+        const blockHash = await client.getBlockHash(height);
 
         // Get block details
         const block = await client.getBlock(blockHash);
 
-        console.log(JSON.stringify(block, null, 4));
+        //console.log(JSON.stringify(block, null, 4));
+
+        console.log(height);
+
+        for (let txnid of block.tx) {
+            await fetchTransaction(txnid);
+        }
     } catch (error) {
         console.error(`Error fetching block: ${error}`);
     }
 }
 
-fetchLatestBlock();
+async function sync() {
+    try {
+        const start = 816793;
+        const blockCount = await client.getBlockCount();
+
+        for (let height = start; height <= blockCount; height++) {
+            await fetchBlock(height);
+        }
+    } catch (error) {
+        console.error(`Error fetching block: ${error}`);
+    }
+}
+
+//fetchBlock(838932);
+
+sync();
