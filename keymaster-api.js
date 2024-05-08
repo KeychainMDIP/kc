8522a9ec9299c003d10df29adbc1d1d8ef38bed9
David McFadzean <davidmc@gmail.com>
2024-05-08 14:41:26 -0400

    Added basic endpoints

--- keymaster-api.js
+++ keymaster-api.js
@@ -2,8 +2,11 @@ import express from 'express';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import * as gatekeeper from './gatekeeper-sdk.js';
+import * as keymaster from './keymaster.js';
 
 const app = express();
+const v1router = express.Router();
 
 app.use(morgan('dev'));
 app.use(express.json());
@@ -13,6 +16,53 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // Serve the React frontend
 app.use(express.static(path.join(__dirname, 'keymaster-app/build')));
+
+v1router.get('/ready', async (req, res) => {
+    try {
+        res.json('ready');
+    } catch (error) {
+        res.status(500).send(error.toString());
+    }
+});
+
+v1router.get('/wallet', async (req, res) => {
+    try {
+        const wallet = keymaster.loadWallet();
+        res.json(wallet);
+    } catch (error) {
+        res.status(500).send(error.toString());
+    }
+});
+
+v1router.get('/ids', async (req, res) => {
+    try {
+        const ids = keymaster.listIds();
+        res.json(ids);
+    } catch (error) {
+        res.status(500).send(error.toString());
+    }
+});
+
+v1router.post('/ids', async (req, res) => {
+    try {
+        const { name, registry } = req.body;
+        const did = await keymaster.createId(name, registry);
+        res.json(did);
+    } catch (error) {
+        res.status(500).send(error.toString());
+    }
+});
+
+v1router.get('/ids/current', async (req, res) => {
+    try {
+        const current = keymaster.getCurrentIdName();
+        res.json(current);
+    } catch (error) {
+        res.status(500).send(error.toString());
+    }
+});
+
+app.use('/api/v1', v1router);
 
 app.use((req, res, next) => {
     if (!req.path.startsWith('/api')) {
@@ -25,6 +75,7 @@ app.use((req, res, next) => {
 
 const port = 7007;
 
-app.listen(port, () => {
+app.listen(port, async () => {
+    await keymaster.start(gatekeeper);
     console.log(`keymaster server running on port ${port}`);
 });
