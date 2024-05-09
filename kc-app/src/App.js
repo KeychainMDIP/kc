3382eddebb9da738106132f78d90acc160c11ed3
David McFadzean <davidmc@gmail.com>
2024-05-09 11:55:58 -0400

    Display current id

--- kc-app/src/App.js
+++ kc-app/src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { Button, TextField, FormControl, InputLabel, Select, MenuItem, Grid } from '@mui/material';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [currentId, setCurrentId] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const getCurrentId = await axios.get(`/api/v1/current-id`);
+        setCurrentId(getCurrentId.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        current user: {currentId}
       </header>
     </div>
   );
