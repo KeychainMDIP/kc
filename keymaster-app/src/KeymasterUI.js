418b9e23afc4aa5ed6752a5907928b72deb9ec00
David McFadzean <davidmc@gmail.com>
2024-06-24 14:23:37 -0400

    Added credential pub features to web UI

--- keymaster-app/src/KeymasterUI.js
+++ keymaster-app/src/KeymasterUI.js
@@ -489,6 +489,33 @@ function KeymasterUI({ keymaster, title }) {
         }
     }
 
+    async function publishCredential(did) {
+        try {
+            await keymaster.publishCredential(did, false);
+            resolveId();
+        } catch (error) {
+            window.alert(error);
+        }
+    }
+
+    async function revealCredential(did) {
+        try {
+            await keymaster.publishCredential(did, true);
+            resolveId();
+        } catch (error) {
+            window.alert(error);
+        }
+    }
+
+    async function unpublishCredential(did) {
+        try {
+            await keymaster.unpublishCredential(did);
+            resolveId();
+        } catch (error) {
+            window.alert(error);
+        }
+    }
+
     async function showMnemonic() {
         try {
             const response = await keymaster.decryptMnemonic();
@@ -991,25 +1018,42 @@ function KeymasterUI({ keymaster, title }) {
                                         <TableBody>
                                             {heldList.map((did, index) => (
                                                 <TableRow key={index}>
-                                                    <TableCell>
-                                                        <Typography style={{ fontSize: '.9em', fontFamily: 'Courier' }}>
+                                                    <TableCell colSpan={6}>
+                                                        <Typography style={{ fontSize: '1em', fontFamily: 'Courier' }}>
                                                             {did}
                                                         </Typography>
-                                                    </TableCell>
-                                                    <TableCell>
-                                                        <Button variant="contained" color="primary" onClick={() => resolveCredential(did)}>
-                                                            Resolve
-                                                        </Button>
-                                                    </TableCell>
-                                                    <TableCell>
-                                                        <Button variant="contained" color="primary" onClick={() => decryptCredential(did)}>
-                                                            Decrypt
-                                                        </Button>
-                                                    </TableCell>
-                                                    <TableCell>
-                                                        <Button variant="contained" color="primary" onClick={() => removeCredential(did)}>
-                                                            Remove
-                                                        </Button>
+                                                        <Grid container direction="row" justifyContent="flex-start" alignItems="center" spacing={3}>
+                                                            <Grid item>
+                                                                <Button variant="contained" color="primary" onClick={() => resolveCredential(did)}>
+                                                                    Resolve
+                                                                </Button>
+                                                            </Grid>
+                                                            <Grid item>
+                                                                <Button variant="contained" color="primary" onClick={() => decryptCredential(did)}>
+                                                                    Decrypt
+                                                                </Button>
+                                                            </Grid>
+                                                            <Grid item>
+                                                                <Button variant="contained" color="primary" onClick={() => removeCredential(did)}>
+                                                                    Remove
+                                                                </Button>
+                                                            </Grid>
+                                                            <Grid item>
+                                                                <Button variant="contained" color="primary" onClick={() => publishCredential(did)}>
+                                                                    Publish
+                                                                </Button>
+                                                            </Grid>
+                                                            <Grid item>
+                                                                <Button variant="contained" color="primary" onClick={() => revealCredential(did)}>
+                                                                    Reveal
+                                                                </Button>
+                                                            </Grid>
+                                                            <Grid item>
+                                                                <Button variant="contained" color="primary" onClick={() => unpublishCredential(did)}>
+                                                                    Unpublish
+                                                                </Button>
+                                                            </Grid>
+                                                        </Grid>
                                                     </TableCell>
                                                 </TableRow>
                                             ))}
