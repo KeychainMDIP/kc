ef204001984b630480f0fb0f647a421c2d1b03aa
David McFadzean <davidmc@gmail.com>
2024-07-25 10:18:29 -0400

    Added file links to keymaster-app

--- src/exceptions.js
+++ src/exceptions.js
@@ -2,4 +2,6 @@ export const INVALID_DID = new Error('Invalid DID');
 export const UNKNOWN_DID = new Error('Unknown DID');
 export const UNKNOWN_ID = new Error('Unknown ID');
 export const INVALID_UPDATE = new Error('Invalid update');
+export const INVALID_OPERATION = new Error('Invalid operation');
+export const INVALID_PARAMETER = new Error('Invalid parameter');
 export const EXPECTED_EXCEPTION = new Error('Expected to throw an exception');
