a922c2e7e075073e3357a83e9eeb59c30fc621ed
David McFadzean <davidmc@gmail.com>
2024-08-20 14:34:19 -0400

    Refactored db-redis

--- src/db-redis.js
+++ src/db-redis.js
@@ -2,14 +2,13 @@ import Redis from 'ioredis';
 import * as exceptions from './exceptions.js';
 
 let redis;
+let DB;
 
-export async function start() {
-    // const url = process.env.KC_REDIS_URL || 'http://localhost:6379';
-    // redis = new Redis(url);
-    redis = new Redis({
-        host: 'localhost',
-        port: 6379
-    });
+export async function start(dbName) {
+    let url = process.env.KC_REDIS_URL || 'redis://localhost:6379';
+
+    redis = new Redis(url);
+    DB = dbName;
 }
 
 export async function stop() {
@@ -17,7 +16,20 @@ export async function stop() {
 }
 
 export async function resetDb() {
-    await redis.flushdb();
+    let cursor = '0';
+    let totalDeleted = 0;
+    do {
+        // Scan for keys that match the pattern
+        const [newCursor, keys] = await redis.scan(cursor, 'MATCH', `${DB}/*`, 'COUNT', 1000);
+        cursor = newCursor;
+
+        if (keys.length > 0) {
+            // Delete the keys found
+            const deletedCount = await redis.del(...keys);
+            totalDeleted += deletedCount; // Increment the total count
+        }
+    } while (cursor !== '0'); // Continue scanning until cursor returns to 0
+    return totalDeleted;
 }
 
 export async function addEvent(did, event) {
@@ -28,7 +40,7 @@ export async function addEvent(did, event) {
     const id = did.split(':').pop();
 
     //console.time('rpush');
-    await redis.rpush(`dids/${id}`, JSON.stringify(event));
+    await redis.rpush(`${DB}/dids/${id}`, JSON.stringify(event));
     //console.timeEnd('rpush');
 }
 
@@ -46,11 +58,11 @@ export async function getEvents(did) {
         throw new Error(exceptions.INVALID_DID);
     }
 
-    console.log(`${did}`);
+    //console.log(`${did}`);
     const id = did.split(':').pop();
-    console.time('lrange');
-    const events = await redis.lrange(`dids/${id}`, 0, -1);
-    console.timeEnd('lrange');
+    //console.time('lrange');
+    const events = await redis.lrange(`${DB}/dids/${id}`, 0, -1);
+    //console.timeEnd('lrange');
     return events.map(event => JSON.parse(event));
 }
 
@@ -60,21 +72,21 @@ export async function deleteEvents(did) {
     }
 
     const id = did.split(':').pop();
-    await redis.del(`dids:${id}`);
+    return redis.del(`${DB}/dids/${id}`);
 }
 
 export async function getAllKeys() {
-    const keys = await redis.keys('dids/*');
-    return keys.map(key => key.split('/')[1]); // Extract the id part from the key
+    const keys = await redis.keys(`${DB}/dids/*`);
+    return keys.map(key => key.split('/').pop()); // Extract the id part from the key
 }
 
 export async function queueOperation(registry, op) {
-    await redis.rpush(`queue:${registry}`, JSON.stringify(op));
+    await redis.rpush(`${DB}/queue/${registry}`, JSON.stringify(op));
 }
 
 export async function getQueue(registry) {
     try {
-        const ops = await redis.lrange(`queue:${registry}`, 0, -1);
+        const ops = await redis.lrange(`${DB}/queue/${registry}`, 0, -1);
         return ops.map(op => JSON.parse(op));
     } catch {
         return [];
@@ -87,9 +99,9 @@ export async function clearQueue(registry, batch) {
         const newOps = ops.filter(op => !batch.some(b => b.signature.value === op.signature.value));
 
         // Clear the current queue and add back the filtered operations
-        await redis.del(`queue:${registry}`);
+        await redis.del(`${DB}/queue/${registry}`);
         if (newOps.length > 0) {
-            await redis.rpush(`queue:${registry}`, ...newOps.map(op => JSON.stringify(op)));
+            await redis.rpush(`${DB}/queue/${registry}`, ...newOps.map(op => JSON.stringify(op)));
         }
 
         return true;
--- src/populate-redis.js
+++ src/populate-redis.js
@@ -3,11 +3,13 @@ import * as db_json from './db-json.js';
 import * as db_redis from './db-redis.js';
 
 await db_json.start('mdip');
-await db_redis.start();
+await db_redis.start('mdip');
 
 async function copyDIDs() {
     const ids = await db_json.getAllKeys();
-    db_redis.resetDb();
+    const deleted = await db_redis.resetDb();
+
+    console.log(`${deleted} keys deleted`);
 
     for (const i in ids) {
         const id = ids[i];
@@ -27,13 +29,52 @@ async function dumpDIDs() {
     }
 }
 
-// console.time('copyDIDs');
-// await copyDIDs();
-// console.timeEnd('copyDIDs');
+async function deleteDIDs() {
+    let ids = await db_redis.getAllKeys();
+    let cache = {};
+
+    while (ids.length > 0) {
+        // Select a random index
+        const i = Math.floor(Math.random() * ids.length);
+
+        // Get the DID at the random index
+        const id = ids[i];
+
+        cache[id] = await db_redis.getEvents(id);
+        await db_redis.deleteEvents(id);
+        console.log(`deleted ${id} ${i}`);
+
+        // Remove the DID from the ids array
+        ids.splice(i, 1);
+    }
+
+    return cache;
+}
+
+async function restoreDIDs(cache) {
+    let keys = Object.keys(cache);
+    for (let i = 0; i < keys.length; i++) {
+        const id = keys[i];
+        await db_redis.setEvents(id, cache[id]);
+        console.log(`${i} ${id} restored`);
+    }
+}
+
+console.time('copyDIDs');
+await copyDIDs();
+console.timeEnd('copyDIDs');
 
 console.time('dumpDIDs');
 await dumpDIDs();
 console.timeEnd('dumpDIDs');
+
+console.time('deleteDIDs');
+const cache = await deleteDIDs();
+console.timeEnd('deleteDIDs');
+
+console.time('restoreDIDs');
+await restoreDIDs(cache);
+console.timeEnd('restoreDIDs');
 
 await db_json.stop();
 await db_redis.stop();
