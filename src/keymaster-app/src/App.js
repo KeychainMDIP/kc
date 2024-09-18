13d749833709481a86f698a75388259f82c44aaf
David McFadzean <davidmc@gmail.com>
2024-09-18 17:00:30 -0400

    Fixed buffer issue

--- src/keymaster-app/src/App.js
+++ src/keymaster-app/src/App.js
@@ -1,9 +1,12 @@
+import { Buffer } from 'buffer';
 import * as gatekeeper from '@macterra/gatekeeper/sdk';
 import * as cipher from '@macterra/cipher/web';
 import * as keymaster from '@macterra/keymaster/lib';
 import * as db_wallet from "@macterra/keymaster/wallet/web";
 import KeymasterUI from './KeymasterUI.js';
 import './App.css';
+
+global.Buffer = Buffer;
 
 function App() {
     keymaster.start(gatekeeper, db_wallet, cipher);
