d5c3c1339adbd7d90523822ce30380e8b0060f4e
David McFadzean <davidmc@gmail.com>
2025-02-06 09:49:23 -0500

    feat: Added export interval to hyperswarm config (#587)

--- services/mediators/hyperswarm/src/config.js
+++ services/mediators/hyperswarm/src/config.js
@@ -7,6 +7,7 @@ const config = {
     gatekeeperURL: process.env.KC_GATEKEEPER_URL || 'http://localhost:4224',
     nodeName: process.env.KC_NODE_NAME || 'anon',
     protocol: process.env.KC_MDIP_PROTOCOL || '/MDIP/v0.6-beta',
+    exportInterval: process.env.KC_HYPR_EXPORT_INTERVAL ? parseInt(process.env.KC_HYPR_EXPORT_INTERVAL) : 2,
 };
 
 export default config;
