c069ff3a176d8e69687eb3dcffe0ae07422e068e
Bushstar <bushsolo@gmail.com>
2025-02-20 13:16:24 +0000

    feat: auto generate OpenAPI docs

---
+++ swaggerConf.js
@@ -0,0 +1,27 @@
+import swaggerJsdoc from 'swagger-jsdoc';
+import fs from 'fs';
+
+const baseDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Keymaster API',
+    version: '1.0.0',
+    description: 'Documentation for Keymaster API'
+  },
+};
+
+const gatekeeperOptions = {
+  failOnErrors: true,
+  definition: {
+    ...baseDefinition,
+    info: { 
+      ...baseDefinition.info, 
+      title: 'Gatekeeper API'
+    }
+  },
+  apis: ['packages/gatekeeper/src/gatekeeper-client.js']
+};
+
+const gatekeeperSpec = swaggerJsdoc(gatekeeperOptions);
+fs.writeFileSync('doc/gatekeeper.json', JSON.stringify(gatekeeperSpec, null, 2));
+
