bb6e5060280eadc451a5d3a3494a7c865c610774
David McFadzean <davidmc@gmail.com>
2025-02-27 10:13:05 -0500

    Updated KC_MDIP_PROTOCOL

--- services/mediators/hyperswarm/src/config.js
+++ services/mediators/hyperswarm/src/config.js
@@ -6,7 +6,7 @@ const config = {
     debug: process.env.KC_DEBUG ? process.env.KC_DEBUG === 'true' : false,
     gatekeeperURL: process.env.KC_GATEKEEPER_URL || 'http://localhost:4224',
     nodeName: process.env.KC_NODE_NAME || 'anon',
-    protocol: process.env.KC_MDIP_PROTOCOL || '/MDIP/v0.6-beta',
+    protocol: process.env.KC_MDIP_PROTOCOL || '/MDIP/v1.0-public',
     exportInterval: process.env.KC_HYPR_EXPORT_INTERVAL ? parseInt(process.env.KC_HYPR_EXPORT_INTERVAL) : 2,
 };
 
