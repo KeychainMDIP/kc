c5277f7e0d5ae0ea8b8fd30246cfef4d3b100b0a
David McFadzean <davidmc@gmail.com>
2025-03-17 10:55:34 -0400

    Added kubo node

---
+++ kubo-test.js
@@ -0,0 +1,74 @@
+import { program } from 'commander';
+import fs from 'fs';
+import { create } from 'kubo-rpc-client'
+
+const ipfs = new create();
+
+program
+    .version('1.0.0')
+    .description('Kubo CLI tool')
+    .configureHelp({ sortSubcommands: true });
+
+program
+    .command('add-string <data>')
+    .description('Add string to IPFS')
+    .action(async (data) => {
+        try {
+            const { cid } = await ipfs.add(data);
+            console.log(cid);
+        }
+        catch (error) {
+            console.error(error);
+        }
+    });
+
+program
+    .command('get-string <cid>')
+    .description('Get content from IPFS')
+    .action(async (cid) => {
+        try {
+            const chunks = [];
+            for await (const chunk of ipfs.cat(cid)) {
+                chunks.push(chunk);
+            }
+            const data = Buffer.concat(chunks);
+            console.log(data.toString());
+        }
+        catch (error) {
+            console.error(error);
+        }
+    });
+
+program
+    .command('add-file <file>')
+    .description('Add file to IPFS')
+    .action(async (file) => {
+        try {
+            const data = fs.readFileSync(file);
+            const { cid } = await ipfs.add(data);
+            console.log(cid);
+        }
+        catch (error) {
+            console.error(error);
+        }
+    });
+
+program
+    .command('get-file <cid> <file>')
+    .description('Get content from IPFS')
+    .action(async (cid, file) => {
+        try {
+            const chunks = [];
+            for await (const chunk of ipfs.cat(cid)) {
+                chunks.push(chunk);
+            }
+            const data = Buffer.concat(chunks);
+            fs.writeFileSync(file, data);
+            console.log(`Data written to ${file}`);
+        }
+        catch (error) {
+            console.error(error);
+        }
+    });
+
+program.parse(process.argv);
