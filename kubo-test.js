f064ce2334b43d6ca4dfa93f374dd94f516c902a
David McFadzean <davidmc@gmail.com>
2025-03-17 11:12:42 -0400

    Added JSON commands

--- kubo-test.js
+++ kubo-test.js
@@ -71,4 +71,38 @@ program
         }
     });
 
+program
+    .command('add-json <file>')
+    .description('Add JSON file')
+    .action(async (file) => {
+        try {
+            const contents = fs.readFileSync(file);
+            const json = JSON.parse(contents.toString());
+            console.log(json);
+            const { cid } = await ipfs.add(JSON.stringify(json));
+            console.log(cid);
+        }
+        catch (error) {
+            console.error(error);
+        }
+    });
+
+program
+    .command('get-json <cid>')
+    .description('Get JSON from IPFS')
+    .action(async (cid) => {
+        try {
+            const chunks = [];
+            for await (const chunk of ipfs.cat(cid)) {
+                chunks.push(chunk);
+            }
+            const data = Buffer.concat(chunks);
+            const json = JSON.parse(data.toString());
+            console.log(JSON.stringify(json, null, 4));
+        }
+        catch (error) {
+            console.error(error);
+        }
+    });
+
 program.parse(process.argv);
