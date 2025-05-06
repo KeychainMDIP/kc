d75e187a838edffbb4256e27c0cbf2ed55287120
David McFadzean <davidmc@gmail.com>
2025-05-06 12:16:53 -0400

    Added openapi docs

--- services/gatekeeper/server/src/gatekeeper-api.ts
+++ services/gatekeeper/server/src/gatekeeper-api.ts
@@ -1713,6 +1713,42 @@ v1router.get('/cas/data/:cid', async (req, res) => {
     }
 });
 
+/**
+ * @swagger
+ * /block/{registry}/latest:
+ *   get:
+ *     summary: Retrieve the latest block for a specific registry
+ *     parameters:
+ *       - in: path
+ *         name: registry
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The name of the registry to retrieve the latest block from.
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved the latest block.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 hash:
+ *                   type: string
+ *                   description: The hash of the latest block.
+ *                 height:
+ *                   type: integer
+ *                   description: The height of the latest block.
+ *                 time:
+ *                   type: integer
+ *                   description: The timestamp of the latest block in seconds since the Unix epoch.
+ *       500:
+ *         description: Internal Server Error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: string
+ */
 v1router.get('/block/:registry/latest', async (req, res) => {
     try {
         const { registry } = req.params;
@@ -1723,6 +1759,57 @@ v1router.get('/block/:registry/latest', async (req, res) => {
     }
 });
 
+/**
+ * @swagger
+ * /block/{registry}/{blockId}:
+ *   get:
+ *     summary: Retrieve a specific block for a given registry
+ *     parameters:
+ *       - in: path
+ *         name: registry
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The name of the registry to retrieve the block from.
+ *       - in: path
+ *         name: blockId
+ *         required: true
+ *         schema:
+ *           oneOf:
+ *             - type: string
+ *               description: The hash of the block.
+ *             - type: integer
+ *               description: The height of the block.
+ *         description: >
+ *           The identifier of the block to retrieve. Can be either a block hash (string) or a block height (integer).
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved the block.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 hash:
+ *                   type: string
+ *                   description: The hash of the block.
+ *                 height:
+ *                   type: integer
+ *                   description: The height of the block.
+ *                 time:
+ *                   type: integer
+ *                   description: The timestamp of the block in seconds since the Unix epoch.
+ *                 timeISO:
+ *                   type: string
+ *                   format: date-time
+ *                   description: The timestamp of the block in ISO 8601 format.
+ *       500:
+ *         description: Internal Server Error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: string
+ */
 v1router.get('/block/:registry/:blockId', async (req, res) => {
     try {
         const { registry, blockId } = req.params;
@@ -1734,6 +1821,53 @@ v1router.get('/block/:registry/:blockId', async (req, res) => {
     }
 });
 
+/**
+ * @swagger
+ * /block/{registry}:
+ *   post:
+ *     summary: Add a new block to a specific registry
+ *     parameters:
+ *       - in: path
+ *         name: registry
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The name of the registry to which the block will be added.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - hash
+ *               - height
+ *               - time
+ *             properties:
+ *               hash:
+ *                 type: string
+ *                 description: The hash of the block.
+ *               height:
+ *                 type: integer
+ *                 description: The height of the block.
+ *               time:
+ *                 type: integer
+ *                 description: The timestamp of the block in seconds since the Unix epoch.
+ *     responses:
+ *       200:
+ *         description: Successfully added the block.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: boolean
+ *               example: true
+ *       500:
+ *         description: Internal Server Error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: string
+ */
 v1router.post('/block/:registry', async (req, res) => {
     try {
         const { registry } = req.params;
