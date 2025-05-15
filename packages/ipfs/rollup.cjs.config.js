1488744146f4f6bef0b23a5a649ad0af2011a99d
Peter John Bushnell <bushsolo@gmail.com>
2025-05-15 16:42:51 +0100

    feat: implement CJS and ESM dual build in packages (#831)
    * feat: implement CJS and ESM dual build in packages
    
    * Resolve GitHub build-and-test warnings
    
    * Correct bundler names

---
+++ packages/ipfs/rollup.cjs.config.js
@@ -0,0 +1,29 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+import pkg from './package.json' with { type: 'json' };
+
+const external = [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {})
+];
+
+const config = {
+    input: {
+        'helia-client': 'dist/esm/helia-client.js',
+        'kubo-client': 'dist/esm/kubo-client.js',
+        utils: 'dist/esm/utils.js'
+    },
+    output: {
+        dir: 'dist/cjs',
+        format: 'cjs',
+        exports: 'named'
+    },
+    external,
+    plugins: [
+        resolve(),
+        commonjs()
+    ]
+};
+
+export default config;
