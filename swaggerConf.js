27c5b704fb5b539bbe3e9f71251ae8bf63036366
David McFadzean <davidmc@gmail.com>
2025-09-29 10:41:31 -0400

    chore: Updated version to 1.3.0 (#1037)

--- swaggerConf.js
+++ swaggerConf.js
@@ -5,7 +5,7 @@ const baseDefinition = {
     openapi: '3.0.0',
     info: {
         title: 'Keymaster API',
-        version: '1.2.1',
+        version: '1.3.0',
         description: 'Documentation for Keymaster API'
     },
 };
