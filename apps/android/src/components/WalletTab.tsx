ded609f8bddfa5d8fbfcb1d2e9d50f17b2fa1b1d
Bushstar <bushsolo@gmail.com>
2025-10-17 07:07:51 +0100

    chore: downloaded mobile/ext wallets should be encrypted

--- apps/android/src/components/WalletTab.tsx
+++ apps/android/src/components/WalletTab.tsx
@@ -260,7 +260,8 @@ const WalletTab = () => {
             return;
         }
         try {
-            const wallet = await keymaster.loadWallet();
+            const wallet_chrome = new WalletWeb();
+            const wallet = await wallet_chrome.loadWallet();
             const walletJSON = JSON.stringify(wallet, null, 4);
             const blob = new Blob([walletJSON], { type: 'application/json' });
             const url = URL.createObjectURL(blob);
