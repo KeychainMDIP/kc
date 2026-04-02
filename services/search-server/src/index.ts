0f730488d3670f31a79927255c1d83923d7da779
Bushstar <bushsolo@gmail.com>
2026-04-02 18:12:59 +0100

    log search-server DB

--- services/search-server/src/index.ts
+++ services/search-server/src/index.ts
@@ -188,6 +188,7 @@ async function main() {
     app.use(express.json({ limit: config.jsonLimit }));
 
     let didDb: DIDsDb;
+    log.info(`Search Server persisting to (${config.db})`);
 
     if (config.db === 'sqlite') {
         didDb = await DIDsSQLite.create();
